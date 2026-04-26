fdf84776c306f28f04b92945affd9111cc62c16b
Maja Schachreiter <maja.schachreiter@gmail.com>
2026-04-26 09:54:53 +0200

    ldfwnbgn

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
 # learning_pytorch
 for my data mining seminar
+I started with this and read until data loaders: https://docs.pytorch.org/tutorials/beginner/basics/tensorqs_tutorial.html
+But I decided to go with the official documentation: https://docs.pytorch.org/tutorials/beginner/basics/intro.html
+
+# Note: This is for arch linux, but it should work on other OS (Mac I don't know)
+# setting up my venv + installing pytorch
+1. python -m venv .venv
+2. source .venv/bin/activate
+3. pip install torch
+Installiere `python3-pip` falls nicht vorhanden, aber nutze es nur innerhalb von Umgebungen: pacman -S python-pip
+
+# to get into your venv again in VS Code
+just go directly in the right folder
+
+# version
+by executing the following script in pytorch, you will find its version:
+```
+import torch
+torch.__version__
+```
+
+# If you have NVIDIA/CUDA-Support:
+After installing PyTorch, you can check whether your installation recognizes your built-in NVIDIA GPU by running the following code in ```python
+import torch torch.cuda.is_available() This returns:
+True
+```
+
---
+++ tensors.py
@@ -0,0 +1,83 @@
+import torch
+import numpy as np
+
+# ------------ 1. init tensor ------------
+# directly from data, type is automatically inferred
+data = [[1, 2],[3, 4]]
+x_data = torch.tensor(data)
+
+# from numpy array
+np_array = np.array(data)
+x_np = torch.from_numpy(np_array)
+
+# from another tensor
+x_ones = torch.ones_like(x_data) # retains the properties of x_data
+print(f"Ones Tensor: \n {x_ones} \n")
+
+x_rand = torch.rand_like(x_data, dtype=torch.float) # overrides the datatype of x_data
+print(f"Random Tensor: \n {x_rand} \n")
+
+# With random or constant values:
+# `shape` is a tuple of tensor dimensions
+shape = (2,3,)
+rand_tensor = torch.rand(shape)
+ones_tensor = torch.ones(shape)
+zeros_tensor = torch.zeros(shape)
+print(f"Random Tensor: \n {rand_tensor} \n")
+print(f"Ones Tensor: \n {ones_tensor} \n")
+print(f"Zeros Tensor: \n {zeros_tensor}")
+
+
+# ------------ 2. attributes of tensor ------------
+# shape, datatype, device they're being stored
+tensor = torch.rand(3,4)
+print(f"Shape of tensor: {tensor.shape}")
+print(f"Datatype of tensor: {tensor.dtype}")
+print(f"Device tensor is stored on: {tensor.device}")
+
+# ------------ 3. operations on tensor ------------
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

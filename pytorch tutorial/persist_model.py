e164309e2aa5d80df92a7b0d7cdf0be4e18bfa42
Maja Schachreiter <maja.schachreiter@gmail.com>
2026-05-11 14:31:05 +0200

    ldfwnbgn

--- pytorch tutorial/persist_model.py
+++ pytorch tutorial/persist_model.py
@@ -29,6 +29,25 @@ torch.save(model, 'model.pth')
 model = torch.load('model.pth', weights_only=False)
 
 
+"""What is state_dict?
+Link[https://docs.pytorch.org/tutorials/beginner/saving_loading_models.html#what-is-a-state-dict]
+learnable parameters (weights and biases) of a Module-class are contained in the model.parameters(). 
+A state_dict is a Python dictionary that maps each layer to its parameter tensor.
+
+- A common PyTorch convention is to save models using either a .pt or .pth file extension.
+- Remember that you must call model.eval() to set dropout and batch normalization layers to evaluation mode before running inference. 
+    Failing to do this will yield inconsistent inference results.
+- model_state = model.state_dict() returns a reference to the state and not its copy! 
+- You must serialize best_model_state or use best_model_state = deepcopy(model.state_dict()) 
+    otherwise your best best_model_state will keep getting updated by the subsequent training iterations.
+Save:
+torch.save(model.state_dict(), PATH)
+Load:
+model = TheModelClass(*args, **kwargs)
+model.load_state_dict(torch.load(PATH, weights_only=True))
+model.eval()
+"""
+
 
 
 
